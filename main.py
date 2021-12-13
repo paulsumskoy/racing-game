d348ddc41eb8ff8fcbb9c17c7d8baeec50505976
paulsumskoy <p.sumskoy@gmail.com>
2021-12-13 14:29:09 +0200

    Update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
+import time
+import math
 from utils import scale_image
 
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.5)
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 
 TRACK_BORDER = scale_image(pygame.image.load("imgs/border.png"), 0.5)
+FINISH = pygame.image.load("imgs/finish.png")
 
 RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
 FREE_CAR = scale_image(pygame.image.load("imgs/free-car.png"), 0.55)
@@ -13,8 +16,19 @@ WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
 
+FPS = 60
+
 run = True
+clock = pygame.time.Clock()
+
 while run: 
+    clock.tick(FPS)
+    
+    WIN.blit(GRASS, (0, 0))
+    WIN.blit(TRACK, (0, 0))
+    WIN.blit(FINISH, (0, 0))
+    
+    pygame.display.update()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
