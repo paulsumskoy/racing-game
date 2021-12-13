48be6fba0320191becf47eaec22dddf246af1478
paulsumskoy <p.sumskoy@gmail.com>
2021-12-13 15:54:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -53,14 +53,15 @@ class AbstractCar:
         
         self.y -= vertical
         self.x -= horizontal
-    
-    def reduce_speed(self):
-        self.vel = max(self.vel - self.acceleration / 2, 0)
-        self.move()
         
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
     START_POS = (180, 200)
+    
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
 
 def draw(win, images, player_car):
     for img, pos in images:
