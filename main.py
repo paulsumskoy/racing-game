94a0aec9258fbd0e7fc8e032be86274e27d6c862
paulsumskoy <p.sumskoy@gmail.com>
2021-12-14 15:00:35 +0200

    Finish

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 
 TRACK_BORDER = scale_image(pygame.image.load("imgs/border.png"), 0.6)
 TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
-FINISH = pygame.image.load("imgs/finish.png")
+FINISH = scale_image(pygame.image.load("imgs/finish.png"), 0.13)
+FINISH_POSITION = (848, 450)
 
 RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.33)
 FREE_CAR = scale_image(pygame.image.load("imgs/free-car.png"), 0.33)
@@ -26,7 +27,7 @@ class AbstractCar:
         self.max_vel = 1.5
         self.vel = 0
         self.rotation_vel = rotation_vel
-        self.angle = 270
+        self.angle = 0
         self.x, self.y = self.START_POS
         self.acceleration = 0.01
         
@@ -63,7 +64,7 @@ class AbstractCar:
         
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
-    START_POS = (200, 610)
+    START_POS = (875, 490)
     
     def reduce_speed(self, slowdown):
         self.vel = max(self.vel - self.acceleration / slowdown, 0)
@@ -103,7 +104,7 @@ def move_player(player_car):
 
 run = True
 clock = pygame.time.Clock()
-images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, FINISH_POSITION)]
 player_car = PlayerCar(4, 4)
 
 while run: 
