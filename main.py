ec8ba9e15bf0a89c194cfbea0daeec61ba1ebab0
paulsumskoy <p.sumskoy@gmail.com>
2021-12-14 15:42:30 +0200

    AI basic

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import time
 import math
 from utils import scale_image, blit_rotate_center
 
-TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.5)
+TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.6)
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 
-TRACK_BORDER = scale_image(pygame.image.load("imgs/border.png"), 0.5)
+TRACK_BORDER = scale_image(pygame.image.load("imgs/border.png"), 0.6)
 TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 
 FINISH = scale_image(pygame.image.load("imgs/finish.png"), 0.13)
@@ -71,7 +71,7 @@ class AbstractCar:
         
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
-    START_POS = (875, 445)
+    START_POS = (860, 445)
     
     def reduce_speed(self, slowdown):
         self.vel = max(self.vel - self.acceleration / slowdown, 0)
@@ -80,13 +80,31 @@ class PlayerCar(AbstractCar):
     def bounce(self):
         self.vel = -self.vel
         self.move()
+        
+class ComputerCar(AbstractCar):
+    IMG = FREE_CAR
+    START_POS = (880, 445)
+    
+    def __init__(self, max_vel, rotation_vel, path=[]):
+        super().__init__(max_vel, rotation_vel)
+        self.path = path
+        self.current_point = 0
+        self.vel = max_vel
+        
+    def draw_points(self, win):
+        for point in self.path:
+            pygame.draw.circle(win, (255, 0, 0), point, 5)
+            
+    def draw(self, win):
+        super().draw(win)
+        self.draw_points(win)
 
-
-def draw(win, images, player_car):
+def draw(win, images, player_car, computer_car):
     for img, pos in images:
         win.blit(img, pos)
     
     player_car.draw(win)
+    computer_car.draw(win)
     pygame.display.update()
     
 def move_player(player_car):
@@ -113,11 +131,12 @@ run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, FINISH_POSITION), (TRACK_BORDER, (0, 0))]
 player_car = PlayerCar(8, 8)
+computer_car = ComputerCar(4, 4)
 
 while run: 
     clock.tick(FPS)
     
-    draw(WIN, images, player_car)
+    draw(WIN, images, player_car, computer_car)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
